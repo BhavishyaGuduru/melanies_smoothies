207cadc811606157480e233674d1597612c578e9
BhavishyaGuduru <bhavishyaguduru7@gmail.com>
2024-07-12 07:52:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,39 +12,46 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on you smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").filter(col("ORDER_FILLED")==0).collect()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON))
+# st.dataframe(data = my_dataframe, use_container_width = True)
+# st.stop()
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
-# my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-# my_dataframe = session.table("smoothies.public.orders").collect()
-if my_dataframe:
-    editable_df = st.data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    if submitted:
-        st.success('Someone clicked the button.')
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-        try:
-            og_dataset.merge(edited_dataset
-                             , (og_dataset['order_uid'] == edited_dataset['order_uid'])
-                             , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                            )
-            st.success('Your Smoothie is ordered!', icon="✅")
-        except:
-            st.write('somethings wrong')
-
-# st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'CHOOSE UP TO 5', my_dataframe
     ,max_selections = 5
 )
 
+
+# # my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+# # my_dataframe = session.table("smoothies.public.orders").collect()
+# if my_dataframe:
+#     editable_df = st.data_editor(my_dataframe)
+#     submitted = st.button('Submit')
+#     if submitted:
+#         st.success('Someone clicked the button.')
+#         og_dataset = session.table("smoothies.public.orders")
+#         edited_dataset = session.create_dataframe(editable_df)
+#         try:
+#             og_dataset.merge(edited_dataset
+#                              , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+#                              , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+#                             )
+#             st.success('Your Smoothie is ordered!', icon="✅")
+#         except:
+#             st.write('somethings wrong')
+
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + '  '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+fruit_chosen)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
